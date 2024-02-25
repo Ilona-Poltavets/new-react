a34f1b1343a3f3f56663f52dee1bc9077ca1419d
Ilona Poltavets <20ilona.poltavets02@gmail.com>
2024-02-25 18:16:35 +0200

    react-move

--- src/App.js
+++ src/App.js
@@ -1,20 +1,64 @@
-import logo from './logo.svg';
+import React, { PureComponent } from 'react';
+import { Animate } from 'react-move';
+import { easeExpOut } from 'd3-ease';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Ilona Poltavets</h1>
-      </header>
-      <main>
-        
-      </main>
-      <footer>
-        
-      </footer>
-    </div>
-  );
+const outerDiv = {
+  borderRadius: 3,
+  backgroundColor: 'rgba(128,128,128)',
+  position: 'relative',
+  margin: '5px 3px 10px',
+  width: 200,
+  height: 40,
+};
+
+class App extends PureComponent {
+  state = {
+    open: false,
+  };
+
+  handleClick = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Toggle</button>
+
+        <Animate
+          start={() => ({
+            x: 0,
+          })}
+          update={() => ({
+            x: [this.state.open ? 200 : 0],
+            timing: { duration: 750, ease: easeExpOut },
+          })}
+        >
+          {(state) => {
+            const { x } = state;
+
+            return (
+              <div style={outerDiv}>
+                <div
+                  style={{
+                    position: 'absolute',
+                    width: 50,
+                    height: 50,
+                    borderRadius: 4,
+                    opacity: 0.7,
+                    backgroundColor: '#ff69b4',
+                    WebkitTransform: `translate3d(${x}px, 0, 0)`,
+                    transform: `translate3d(${x}px, 0, 0)`,
+                  }}
+                />
+              </div>
+            );
+          }}
+        </Animate>
+      </div>
+    );
+  }
 }
 
 export default App;
